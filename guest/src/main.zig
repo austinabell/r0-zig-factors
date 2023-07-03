2b0d76a5019d64a5fe8e6a8f591bd0bd891cfae5
Austin Abell <austinabell8@gmail.com>
2023-07-03 10:11:15 -0400

    update sha initial state

--- guest/src/main.zig
+++ guest/src/main.zig
@@ -14,14 +14,23 @@ export fn _start() callconv(.Naked) noreturn {
 
 fn sys_halt() noreturn {
 	// TODO pull hash state instead of zeroed
-	const out_state = [_]u32{0} ** 8;
+	const out_state = [_]u32{
+		0x6a09e667,
+		0xbb67ae85,
+		0x3c6ef372,
+		0xa54ff53a,
+		0x510e527f,
+		0x9b05688c,
+		0x1f83d9ab,
+		0x5be0cd19,
+	};
 	asm volatile (
 		\\ ecall
 		:
 		: [syscallNumber] "{t0}" (ECALL_HALT),
 		// NOTE: rust code does bit OR with the exit code, but in code is always 0, so ignored
 		[haltCode] "{a0}" (HALT_TERMINATE),
-		[outState] "{a1}" (out_state)
+		[outState] "{a1}" (&out_state)
 		: "memory"
 	);
 	unreachable;
