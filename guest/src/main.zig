b4062f1b051e4005fe4de0108bfacd1ee7b56af2
Austin Abell <austinabell8@gmail.com>
2023-07-05 14:03:23 -0400

    connect logic

--- guest/src/main.zig
+++ guest/src/main.zig
@@ -19,15 +19,24 @@ const INITIAL_SHA_STATE = [_]u32{
 };
 
 export fn _start() callconv(.Naked) noreturn {
-    // TODO logic
-	// Create buffer large enough for two u64 values.
-	// This could be done separately, but it saves a syscall to read all words at once.
-	var buffer = [_]u8{0} ** 16;
-	sys_read(FILENO_STDIN, 16, buffer);
+    // Create buffer large enough for two u64 values.
+    // This could be done separately, but it saves a syscall to read all words at once.
+    var buffer = [_]u8{0} ** 16;
+    sys_read(FILENO_STDIN, 16, buffer);
+    const a: u64 = std.mem.bytesAsValue(u64, buffer[0..8]).*;
+    const b: u64 = std.mem.bytesAsValue(u64, buffer[8..16]).*;
+    if (a == 1 or b == 1) {
+        @panic("This is a panic message!\n");
+    }
 
-	const serialized_value = std.mem.asBytes(&8);
-	sys_write(serialized_value);
-	var sha_state = sys_sha_buffer(serialized_value, INITIAL_SHA_STATE);
+    var product: u64 = undefined;
+    if (@mulWithOverflow(u64, a, b, &product)) {
+        @panic("Integer overflow");
+    }
+
+    const serialized_value = std.mem.asBytes(&product);
+    sys_write(serialized_value);
+    var sha_state = sys_sha_buffer(serialized_value, INITIAL_SHA_STATE);
 
     sys_halt(sha_state);
 }
@@ -48,17 +57,17 @@ fn sys_halt(out_state: [8]u32) noreturn {
 fn sys_sha_buffer(data: []u8, in_state: [8]u32) [8]u32 {
     // TODO this assumes that the data is unaligned and that there is not enough for a full block
     // for the sake of this program it's fine, but logic will be different if the data is larger
-	// than the block size.
+    // than the block size.
     var buffer: [8]u32 = undefined;
 
     // TODO this is assuming a single block, without length terminated padding, zero padded.
     // This works for this program, but will need to be changed if used ambiguously.
     var hash_block: [16]u32 = [_]u32{0} ** 16;
-	// TODO see if can just use a u8 buffer and send pointer as if u32 array
+    // TODO see if can just use a u8 buffer and send pointer as if u32 array
     var bytes = @ptrCast([*]u8, &hash_block);
 
     // Copy whole bytes
-    var len = std.math.min(data.len, hash_block.len*4);
+    var len = std.math.min(data.len, hash_block.len * 4);
     std.mem.copy(u8, bytes[0..len], data[0..len]);
 
     asm volatile (
@@ -77,9 +86,9 @@ fn sys_sha_buffer(data: []u8, in_state: [8]u32) [8]u32 {
 }
 
 fn sys_write(data: []u8) void {
-	const syscall_name: [:0]const u8 = "nr::SYS_WRITE";
-	asm volatile (
-		\\ ecall
+    const syscall_name: [:0]const u8 = "nr::SYS_WRITE";
+    asm volatile (
+        \\ ecall
         :
         : [syscallNumber] "{t0}" (ECALL_SOFTWARE),
           [from_host] "{a0}" (&data),
@@ -95,10 +104,10 @@ fn sys_write(data: []u8) void {
 fn sys_read(fd: u32, comptime nrequested: usize, buffer: [nrequested]u8) void {
     const main_words = nrequested / 4;
 
-	const syscall_name: [:0]const u8 = "nr::SYS_READ";
-	asm volatile (
+    const syscall_name: [:0]const u8 = "nr::SYS_READ";
+    asm volatile (
         \\ ecall
-		:
+        :
         // : [out_a0] "={a0}" (a0) // NOTE: probably don't need to know the amount read
         : [syscallNumber] "{t0}" (ECALL_SOFTWARE),
           [from_host] "{a0}" (&buffer),
