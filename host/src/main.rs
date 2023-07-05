e90cbd01aaee8eae4a898cdacb327be29e18b6d5
Austin Abell <austinabell8@gmail.com>
2023-07-05 15:05:23 -0400

    update host execution

--- host/src/main.rs
+++ host/src/main.rs
@@ -2,7 +2,7 @@
 // is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
 // `METHOD_NAME_ID` with `MULTIPLY_ID`
 use risc0_zkvm::{
-    // serde::{from_slice, to_vec},
+    serde::{from_slice, to_vec},
     Executor, ExecutorEnv,
 };
 
@@ -11,8 +11,8 @@ const BYTES: &[u8] = include_bytes!("../../guest/zig-out/bin/factors");
 fn main() {
     // First, we construct an executor environment
     let env = ExecutorEnv::builder()
-        .add_input(&[0u8; 31])
-        .add_input(&[0u8; 33])
+        .add_input(&to_vec(&17u64).unwrap())
+        .add_input(&to_vec(&23u64).unwrap())
         .build();
 
     // Next, we make an executor, loading the (renamed) ELF binary.
@@ -22,7 +22,14 @@ fn main() {
     let session = exec.run().unwrap();
 
     // Prove the session to produce a receipt.
-    let _receipt = session.prove().unwrap();
+    let receipt = session.prove().unwrap();
+
+    // Extract journal of receipt (i.e. output c, where c = a * b)
+    let c: u64 = from_slice(&receipt.journal).expect(
+        "Journal output should deserialize into the same types (& order) that it was written",
+    );
+
+    assert_eq!(c, 17 * 23);
 
     // TODO: Implement code for transmitting or serializing the receipt for
     // other parties to verify here
